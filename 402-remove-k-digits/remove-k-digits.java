8e7b5ed32662683d38ffca60e6c93b7548005b3b
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-08-15 19:16:14 +0530

    Time: 24 ms (48.36%) | Memory: 47.8 MB (47.96%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,39 +1,76 @@
 class Solution {
     public String removeKdigits(String num, int k) {
-        StringBuilder sb= new StringBuilder(num);
-        int n=num.length();
-        int i=0;
-        if(n == k) return "0";
-         while(k > 0 && sb.length() > 1 && i<sb.length()-1){
-            int first= sb.charAt(i)-'0';
-            int sec = sb.charAt(i+1)-'0';
-            if (first > sec) {
-                sb.deleteCharAt(i);
-                k--;
 
-                if (i > 0) {
-                    i--;
-                }
-            } 
-            else {
-                i++;
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < num.length(); i++) {
+            while (!st.isEmpty() && k > 0 &&
+                   st.peek() > num.charAt(i)) {
+                st.pop();
+                k--;
             }
-            
-         }
-           while (k > 0) {
-            sb.deleteCharAt(sb.length() - 1);
+            st.push(num.charAt(i));
+        }
+
+        while (k > 0) {
+            st.pop();
             k--;
         }
-         while(sb.length() > 0 && sb.charAt(0) == '0'){
-            sb.deleteCharAt(0);
-         }
-         if(sb.length() == 0){
-            return "0";
-         }
-         return String.valueOf(sb);
 
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
+        }
+        res.reverse();
+
+        while (res.length() > 1 && res.charAt(0) == '0') {
+            res.deleteCharAt(0);
+        }
+        if (res.length() == 0) {
+            return "0";
+        }
+
+        return res.toString();
     }
 }
+
+
+// class Solution {
+//     public String removeKdigits(String num, int k) {
+//         StringBuilder sb= new StringBuilder(num);
+//         int n=num.length();
+//         int i=0;
+//         if(n == k) return "0";
+//          while(k > 0 && sb.length() > 1 && i<sb.length()-1){
+//             int first= sb.charAt(i)-'0';
+//             int sec = sb.charAt(i+1)-'0';
+//             if (first > sec) {
+//                 sb.deleteCharAt(i);
+//                 k--;
+
+//                 if (i > 0) {
+//                     i--;
+//                 }
+//             } 
+//             else {
+//                 i++;
+//             }
+            
+//          }
+//            while (k > 0) {
+//             sb.deleteCharAt(sb.length() - 1);
+//             k--;
+//         }
+//          while(sb.length() > 0 && sb.charAt(0) == '0'){
+//             sb.deleteCharAt(0);
+//          }
+//          if(sb.length() == 0){
+//             return "0";
+//          }
+//          return String.valueOf(sb);
+
+//     }
+// }
 
 
 
