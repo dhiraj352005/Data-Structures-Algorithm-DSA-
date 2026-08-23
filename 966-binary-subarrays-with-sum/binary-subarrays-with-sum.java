c7364985d59bb57f79d8fc955b113bdc9ef7174e
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-08-23 19:02:16 +0530

    Time: 1 ms (99.96%) | Memory: 50.9 MB (66.30%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,3 +1,56 @@
+// Most Optimal Solution 
+// T.C = O(2N + 2N)
+// S.C=O(1)
+
+class Solution {
+    public int fun(int[]nums, int k){
+       int n=nums.length;
+       int count=0;
+       int sum=0;
+       int l=0, r=0;
+
+       if(k < 0) return 0;   // sum < 0 not possible in this question bcoz all elements >= 0
+
+       while(r<n){      // t.c = O(N)
+        sum += nums[r];
+
+        while(sum>k ){     // this loop rums overall in t.c = O(N) , Not for individual 
+            sum -= nums[l];
+            l++;
+        }
+        count += (r-l+1);
+        r++;
+       }
+       return count;
+    }
+
+
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        /* No. of subarrays where (sum = k)      // ex. Sum 10 = (Sum <= 10) - (Sum <= 9)
+           = 
+           No. of subarrays where (sum <= k)
+           -
+           No. of subarrays where (sum <= k-1)
+        */
+        int ans = fun(nums, goal) -  fun(nums, goal-1);     // t.C = (2N) + (2N)  and space = 1
+       
+        return ans;
+    }
+}
+
+
+
+
+
+
+// Better Approach 
+
+/* 
+// Prefix sum+ Hashmap 
+
+// T.C = O(N)
+// S.C=O(N)
+
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int count=0, n=nums.length, sum=0;
@@ -17,6 +70,7 @@ class Solution {
     }
 }
 
+*/
 
 
 
