812263f607dae996c748c5016d019f9d64f01575
Dhiraj Raut <159889522+dhiraj352005@users.noreply.github.com>
2026-09-13 23:22:30 +0530

    Time: 18 ms (98.48%) | Memory: 44.9 MB (52.65%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -6,7 +6,7 @@ class Solution {
         List<int[]> B=new ArrayList<>();
         for(int i=0;i<n;++i){
              for(int j=0;j<n;++j){
-                if(img1[i][j]==1) A.add(new int[]{i,j});
+                if(img1[i][j]==1) A.add(new   int[]{i,j});
                  if(img2[i][j]==1) B.add(new int[]{i,j});
             }
         }
@@ -15,9 +15,9 @@ class Solution {
 
     for(int[] a:A){
             for(int[] b:B){
-                 int dx=b[0]-a[0]+n;
+                 int dx=b[0]- a[0]+n;
                 int dy=b[1]-a[1]+n;
-                best=Math.max(best,++cnt[dx][dy]);
+                best=Math.max(best, ++cnt[dx][dy]);
             }
         }
 
